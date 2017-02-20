20413b92e7dd038e14489b9d5b0cf44155efaee9
spitsfire <yue.katou@gmail.com>
2017-02-19 19:31:02 -0600

    Added second method to create a new contact within contact list.

--- contact-list/contactList.rb
+++ contact-list/contactList.rb
@@ -1,13 +1,25 @@
 #create contact list array
 contact_list = []
 
-#create method to ask the user different questions
+#method to ask the user different questions
 def ask(question, kind="string")
-print question + " "
-answer = gets.chomp
-answer = answer.to_i if kind == "number"
-return answer
+	print question + " "
+	answer = gets.chomp
+	answer = answer.to_i if kind == "number"
+	return answer
 end
 
-answer = ask("What is your name?")
-puts answer
+#method to create a new contact
+def add_contact
+	contact = {"name" => "", "phone_numbers" => []}
+	contact["name"] = ask("What is the person's name?")
+	answer = ""
+	while answer != "n"
+		answer = ask("Do you want to add a phone number? (y/n)")
+		if answer == "y"
+			phone = ask("Enter a phone number: ")
+			contact["phone_numbers"].push(phone)
+		end
+	end
+	return contact
+end
